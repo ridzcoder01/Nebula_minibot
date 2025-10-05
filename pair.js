c083d07e28ff4f5b47982f299cb50380f20c0f1b
ridzcoder01 <smtechofcmodz@outlook.com>
2025-10-05 13:07:28 +0300

    Update pair.js

--- pair.js
+++ pair.js
@@ -1318,11 +1318,11 @@ function setupCommandHandlers(socket, number) {
     const yts = require('yt-search');
     const axios = require('axios');
 
-    function extractYouTubeId(url) {
-        const regex = /(?:https?:\/\/)?(?:www\.)?(?:youtube\.com\/(?:watch\\?v=|embed\\/|v\\/|shorts\\/)|youtu\\.be\\/)([a-zA-Z0-9_-]{11})/;
-        const match = url.match(regex);
-        return match ? match[1] : null;
-    }
+  function extractYouTubeId(url) {
+    const regex = /(?:https?:\/\/)?(?:www\.)?(?:youtube\.com\/(?:watch\?v=|embed\/|v\/|shorts\/)|youtu\.be\/)([a-zA-Z0-9_-]{11})/;
+    const match = url.match(regex);
+    return match ? match[1] : null;
+}
 
     function convertYouTubeLink(input) {
         const videoId = extractYouTubeId(input);
